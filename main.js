b385a84284f06d573f96a2f26a9d60122c6e3a85
Ehikhamele Gift <144893520+ehikhamelegift@users.noreply.github.com>
2024-01-28 14:06:46 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const left = document.querySelector('.split-left');
+const right = document.querySelector('.split-right');
+const container = document.querySelector('.container');
+const buttonEL = document.querySelector('.button');
+
+left.addEventListener('mouseenter', () => {
+    container.classList.add('hover-left')
+});
+
+left.addEventListener('mouseleave', () => {
+    container.classList.remove('hover-left')
+});
+
+right.addEventListener('mouseenter', () => {
+    container.classList.add('hover-right')
+});
+
+right.addEventListener('mouseleave', () => {
+    container.classList.remove('hover-right')
+});
